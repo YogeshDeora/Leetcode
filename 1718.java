c8011c2772308a36f9fe4620a8f93097e1d34daf
Yogesh Deora <147099646+YogeshDeora@users.noreply.github.com>
2025-02-16 20:05:22 +0530

    Create 1718.java

---
+++ 1718.java
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/construct-the-lexicographically-largest-valid-sequence/?envType=daily-question&envId=2025-02-16
+class Solution {
+    public int[] constructDistancedSequence(int n) {
+        int[] result = new int[2 * n - 1];
+        boolean[] used = new boolean[n + 1];
+        backtrack(result, used, 0, n);
+        return result;
+    }
+
+    private boolean backtrack(int[] result, boolean[] used, int index, int n) {
+        if (index == result.length) {
+            return true;
+        }
+        if (result[index] != 0) {
+            return backtrack(result, used, index + 1, n);
+        }
+        for (int i = n; i >= 1; i--) {
+            if (used[i]) continue;
+            if (i == 1) {
+                result[index] = 1;
+                used[1] = true;
+                if (backtrack(result, used, index + 1, n)) return true;
+                result[index] = 0;
+                used[1] = false;
+            } else {
+                if (index + i < result.length && result[index] == 0 && result[index + i] == 0) {
+                    result[index] = i;
+                    result[index + i] = i;
+                    used[i] = true;
+                    if (backtrack(result, used, index + 1, n)) return true;
+                    result[index] = 0;
+                    result[index + i] = 0;
+                    used[i] = false;
+                }
+            }
+        }
+        return false;
+    }
+}
