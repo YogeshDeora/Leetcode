644129edc5b389b87b4ba4c1a8e7d471658b0880
Yogesh Deora <147099646+YogeshDeora@users.noreply.github.com>
2025-04-03 17:45:20 +0530

    Create 2874.java

---
+++ 2874.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-ii/description/?envType=daily-question&envId=2025-04-03
+
+class Solution {
+    public long maximumTripletValue(int[] nums) {
+        int maxi = Integer.MIN_VALUE;
+        int diff = 0;
+        long res = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            maxi = Math.max(maxi, nums[i]);
+            if (i >= 2)
+                res = Math.max(res, (long)diff * nums[i]);
+            if (i >= 1)
+                diff = Math.max(diff, maxi - nums[i]);
+        }
+        
+        return res;
+    }
+}
