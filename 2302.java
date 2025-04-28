1907bd053b741f650ef276e7f69d7348161b2945
Yogesh Deora <147099646+YogeshDeora@users.noreply.github.com>
2025-04-28 10:45:23 +0530

    Create 2302.java

---
+++ 2302.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/count-subarrays-with-score-less-than-k/description/?envType=daily-question&envId=2025-04-28
+
+class Solution {
+    public long countSubarrays(int[] nums, long k) {
+        long res = 0, partSum = 0;
+        int left = 0;
+        
+        for (int right = 0; right < nums.length; right++) {
+            partSum += nums[right];
+            while (partSum * (right - left + 1) >= k) {
+                partSum -= nums[left++];
+            }
+            res += right - left + 1;
+        }
+        
+        return res;
+    }
+}
